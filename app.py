4a8a79e700fcd03d81704089fb653a1b4e5a9e86
yoohyun_jung <yoohyun.jung@sfchronicle.com>
2022-08-17 10:32:05 -0700

    added calculation

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ df = pd.read_csv(url)
 
 # save to CSV
 df.to_csv('./data/USmap_counts.csv')
+
+# call state population data
+pop = pd.read_csv('./data/state_pop.csv')
+
+# join case data with population
+dfpop = pd.merge(df, pop, left_on='Location', right_on='State')
+
+# calculate per 100K rate and put in new column
+dfpop['per100k'] = dfpop['Cases'] / dfpop['pop_est_2021'] *100000
+
+# save to CSV
+dfpop.to_csv('./data/state_mp_per100k.csv')
