07deab2d12998890e4a520a328d377d673f54066
yoohyun_jung <yoohyun.jung@sfchronicle.com>
2022-08-17 10:40:31 -0700

    metadata added

--- app.py
+++ app.py
@@ -20,3 +20,18 @@ dfpop['per100k'] = dfpop['Cases'] / dfpop['pop_est_2021'] *100000
 
 # save to CSV
 dfpop.to_csv('./data/state_mp_per100k.csv')
+
+# create metadata file 
+def createMetadata():
+    print('Creating metadata...')
+    # Save current date to variable in this format: Aug. 4, 2022
+    date = datetime.datetime.now().strftime("%b. %-d, %Y")
+    s = f'Data as of {date}'
+    data = {}
+
+    # Create nested dictionary with metadata
+    data['annotate'] = {'notes': s}
+
+    json_data = json.dumps(data)
+    with open('data/metadata.json', 'w') as f:
+        f.write(json_data)
